87c28f5fc72f8576fdeeba4118f4ad7cc929657f
ai_ern <im_in@outlook.sg>
2021-02-02 12:21:02 +0800

    progression step bar

--- progress bar/script.js
+++ progress bar/script.js
@@ -1,14 +1,48 @@
-// const items = document.querySelectorAll(".panel");
+const progress = document.getElementById("progress");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const circles = document.querySelectorAll(".circle");
 
-// items.forEach((item) => {
-//   item.addEventListener("click", () => {
-//     removeActiveClasses();
-//     item.classList.add("active");
-//   });
-// });
+let currentActive = 1;
 
-// function removeActiveClasses() {
-//   items.forEach((item) => {
-//     item.classList.remove("active");
-//   });
-// }
+next.addEventListener("click", () => {
+  currentActive++;
+  if (currentActive > circles.length) {
+    currentActive = circles.length;
+  }
+  update();
+});
+
+prev.addEventListener("click", () => {
+  currentActive--;
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+
+  update();
+});
+
+function update() {
+  circles.forEach((circle, idx) => {
+    // console.log(idx, currentActive);
+    if (idx < currentActive) {
+      circle.classList.add("active");
+    } else {
+      circle.classList.remove("active");
+    }
+  });
+
+  const actives = document.querySelectorAll(".active");
+
+  progress.style.width =
+    ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
+
+  if (currentActive === 1) {
+    prev.disabled = true;
+  } else if (currentActive === circles.length) {
+    next.disabled = true;
+  } else {
+    prev.disabled = false;
+    next.disabled = false;
+  }
+}
