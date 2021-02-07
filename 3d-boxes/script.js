3b5dc1e06a20e495e622a05cf7ee4a3926ef21c3
ai_ern <im_in@outlook.sg>
2021-02-07 11:16:49 +0800

    added toggle of classList big upon click

--- 3d-boxes/script.js
+++ 3d-boxes/script.js
@@ -1,14 +1,16 @@
 const boxesContainer = document.getElementById("boxes");
-const btn = document.getElementById("btn");
+const button = document.getElementById("btn");
 
-btn.addEventListener("click", () => boxesContainer.classList.toggle("big"));
+button.addEventListener("click", () => boxesContainer.classList.toggle("big"));
 
 function createBoxes() {
   for (let i = 0; i < 4; i++) {
     for (let j = 0; j < 4; j++) {
+      // console.log(-i*125, -j*125);
       const box = document.createElement("div");
       box.classList.add("box");
       box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`;
+
       boxesContainer.appendChild(box);
     }
   }
